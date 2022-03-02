4f29e0fb4cf023c1aead3d194c6547d3705583bc
Majharul tanvir <mdtanvir6070@gmail.com>
2022-03-02 22:49:02 +0600

    correction

--- js/phone-finder.js
+++ js/phone-finder.js
@@ -33,9 +33,9 @@ const viewPhone = (phones) => {
             const div = document.createElement('div');
             div.innerHTML = `
             <div class="col">
-                    <div class="card h-100 align-items-center">
-                        <img src="${phone.image}" class="card-img-top img-fluid my-2 w-50" alt="...">
-                        <div class="card-body">
+                    <div class="card h-100 shadow bg-white rounded" align-items-center">
+                        <img src="${phone.image}" class="card-img-top img-fluid m-auto my-2 w-50" alt="...">
+                        <div class="card-body m-auto">
                             <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">${phone.brand}</p>
                         <button onclick="detailsPhone('${phone.slug}')" class="bg-primary rounded border-0 text-light px-3">More details</button>
@@ -72,22 +72,22 @@ const detailsSet = (mainDetails) => {
     const detailSet = document.getElementById("phone-details");
     const div = document.createElement('div')
     div.innerHTML = `
-        <div class="card" text-align-center style="width: 18rem;">
-                    <img class="card-img-top img-fluid w-75 m-auto my-3" src="${mainDetails.image}" alt="Card image cap">
+        <div class="card justify-content-center mx-auto">
+                    <img class="card-img-top img-fluid w-50 m-auto my-3" src="${mainDetails.image}" alt="Card image cap">
                     <div class="card-body">
                       <h5 class="card-title">${mainDetails.name}</h5>
                       <p class="card-text">${mainDetails.releaseDate}</p>
                     </div>
-                    <h6 class="m-3">Main Features</h6>
+                    <h6 class="mx-3">Main Features</h6>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item"><b>Storage:</b> ${mainDetails.mainFeatures.storage}</li>
                     <li class="list-group-item"><b>Chipset:</b> ${mainDetails.mainFeatures.chipSet}</li>
                     <li class="list-group-item"><b>Display size:</b> ${mainDetails.mainFeatures.displaySize}</li>
                     <li class="list-group-item"><b>Memory:</b> ${mainDetails.mainFeatures.memory}</li>
-                    <h6 class="m-3">Sensore</h6>
+                    <h6 class="mx-3">Sensore</h6>
                     <li class="list-group-item"><b>Sensor:</b> ${mainDetails.sensors}</li>
                     </ul>
-                    <h6 class="m-3">Others info</h6>
+                    <h6 class="mx-3">Others info</h6>
                     <ul class="list-group m-2 list-group-flush">
                       <li class="list-group-item"><b>Bluetooth:</b> ${mainDetails.others.Bluetooth}</li>
                       <li class="list-group-item"><b>GPS:</b> ${mainDetails.others.GPS}</li>
